0752a8a0e158d159b675a101b108cdb67f084598
Epicentr_K KH <nataliiayeromina3001@icloud.com>
2021-12-10 13:38:04 +0200

    and file

---
+++ лекция5.py
@@ -0,0 +1,51 @@
+l = [1,2,3,1,2,3,5,6,45,34,22,1,2,3,102.3,7]
+new_l = []
+for x in l:
+    if x not in new_l:
+        new_l.append(x)
+print(new_l)
+
+
+# s = [2,4,5,7,2]
+# new_s = list(s)
+# print(new_s)
+
+
+# s1 = [1,2,6,5,4]
+# s2 = [1,2,9,8,7]
+# s = []
+# for i in s1:
+#     if i not in s2:
+#         s.append(i)
+# l = []
+# for i in s2:
+#     if i not in s1:
+#         l.append(i)
+# print(s+l)
+
+
+#
+# d = {1:10, 2:20}
+# d[3] = 30
+# d[4] = 40
+# d[5] = 50
+# d[6] = 60
+# print(d)
+
+#     OR
+#
+# d1 = {1:10, 2:20}
+# d2 = {3:30, 4:40}
+# d3 = {5:50, 6:60}
+# merge = {**d1,**d2,**d3}
+# print(merge)
+
+
+#
+# d = {}
+# for a in range(1,16):
+#     d[a] = a * a
+#
+# print(d)
+
+
